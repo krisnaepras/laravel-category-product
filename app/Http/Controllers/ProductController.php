261747e4342f049909f37bddbceaead8a8324c6c
krisnaepras <krisnaepras@gmail.com>
2025-01-03 17:33:47 +0700

    init project, add tabel, add feature create & delete

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Str;
+use Yajra\DataTables\DataTables;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('products.index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductRequest $request):JsonResponse
+    {
+        $data = $request->validated();
+
+        $data['slug'] = Str::slug($data['name']);
+        Product::create($data);
+
+        return response()->json(['message' => 'Product created successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return response()->json([
+            'data' => Product::find($id)
+        ]);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Product::destroy($id);
+
+        return response()->json(['message' => 'Product deleted successfully']);
+
+    }
+
+    public function serversideTable(Request $request)
+    {
+        $product = Product::get();
+
+        return DataTables::of($product)
+        ->addIndexColumn()
+        ->addColumn('action', function ($row) {
+            return '<div class="text-center">
+                        <button class="btn btn-sm btn-success" onclick="editModal(this)" data-id="' . $row->id . '">Edit</button>
+                        <button class="btn btn-sm btn-danger" onclick="deleteModal(this)" data-id="' . $row->id . '">Delete</button>
+                    </div>';
+        })
+        ->make();
+    }
+}
